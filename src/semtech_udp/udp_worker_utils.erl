48f3fe22824a0c189836d70d058e31b31cd7a17a
Jon Ruttenberg <ruttenberg@stanfordalumni.org>
2022-07-19 14:00:31 -0400

    Add udp worker utils.

---
+++ src/semtech_udp/udp_worker_utils.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author jonathanruttenberg
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 19. Jul 2022 12:58 PM
+%%%-------------------------------------------------------------------
+-module(udp_worker_utils).
+-author("jonathanruttenberg").
+
+%% API
+-export([]).
