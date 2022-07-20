d05650b5b3d5b60ea60a67acdfe745fbe0f97040
Jon Ruttenberg <ruttenberg@stanfordalumni.org>
2022-07-20 13:36:54 -0400

    More code from packet-purchaser.

--- src/semtech_udp/udp_worker_utils.erl
+++ src/semtech_udp/udp_worker_utils.erl
@@ -14,7 +14,7 @@
 %% API
 -export([handle_push_data/3, pubkeybin_to_mac/1, send_tx_ack/2,
   handle_pull_response/3, handle_pull_ack/6, handle_pull_data_timeout/3,
-  send_pull_data/1, schedule_pull_data/1]).
+  send_pull_data/1, schedule_pull_data/1, handle_push_ack/4]).
 
 handle_push_data(PushDataMap, Location, PacketTime) ->
   #{pub_key_bin := PubKeyBin,
@@ -134,3 +134,16 @@ send_pull_data(
       Error
   end.
 
+handle_push_ack(Data, PushData, NetID, MetricsPrefix) ->
+  Token = semtech_udp:token(Data),
+  case maps:get(Token, PushData, undefined) of
+    undefined ->
+      lager:debug("got unknown push ack ~p", [Token]),
+      PushData;
+    {_, TimerRef} ->
+      lager:debug("got push ack ~p", [Token]),
+      _ = erlang:cancel_timer(TimerRef),
+      ok = gwmp_metrics:push_ack(MetricsPrefix, NetID),
+      NewPushData = maps:remove(Token, PushData),
+      NewPushData
+  end.
